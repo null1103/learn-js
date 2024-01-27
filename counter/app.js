2083382c44f08726f100ac34bcecd9aef46c2a45
SK <12840358+null1103@users.noreply.github.com>
2024-01-26 22:07:19 -0800

    Counter app using KO

--- counter/app.js
+++ counter/app.js
@@ -1,1 +1,17 @@
-console.log(ko.version);
+function viewModel() {
+  const self = this;
+  self.counter = ko.observable(0);
+  self.increase = function () {
+    let currentVal = self.counter();
+    self.counter(currentVal + 1);
+  };
+  self.decrease = function () {
+    let currentVal = self.counter();
+    self.counter(currentVal - 1);
+  };
+  self.reset = function () {
+    self.counter(0);
+  };
+}
+
+ko.applyBindings(viewModel, document.getElementById("knockout-app"));
